63002ac6ff9833a15af5f5e102a44bf9668ecf24
hyena0608 <goatsfish@gmail.com>
2022-03-11 13:04:21 +0900

    gen 구현 1차

---
+++ miniminishop/src/main/java/com/example/miniminishop/MiniminishopApplication.java
@@ -0,0 +1,12 @@
+package com.example.miniminishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniminishopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniminishopApplication.class, args);
+	}
+}
