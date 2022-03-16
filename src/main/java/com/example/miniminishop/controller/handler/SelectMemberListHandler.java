138c04b3675edb8e63af4f072ec20e04a9f252c2
hyena0608 <goatsfish@gmail.com>
2022-03-16 13:13:36 +0900

    feat(MemberHandler): 회원 리스트 조회 기능 구현

--- src/main/java/com/example/miniminishop/controller/handler/SelectMemberListHandler.java
+++ src/main/java/com/example/miniminishop/controller/handler/SelectMemberListHandler.java
@@ -2,21 +2,34 @@ package com.example.miniminishop.controller.handler;
 
 import com.example.miniminishop.controller.request.*;
 import com.example.miniminishop.controller.response.*;
+import com.example.miniminishop.controller.util.Converter;
+import com.example.miniminishop.controller.vo.Member;
+import com.example.miniminishop.mapper.MiniminishopMapperService;
+import com.example.miniminishop.mapper.vo.MemberVo;
 import com.example.miniminishop.service.CustomUserDetails;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import com.example.miniminishop.controller.ResultCode;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class SelectMemberListHandler extends BaseHandler {
+
+  private final MiniminishopMapperService mapperService;
 
   public SelectMemberListResponse execute(CustomUserDetails user, SelectMemberListRequest req) {
     SelectMemberListResponse res = new SelectMemberListResponse();
 
     try {
-      // code here
+
+      List<MemberVo> findMemberVoList = mapperService.selectMemberList();
+      List<Member> findMemberList = findMemberVoList.stream().map(o -> Converter.convert(o, Member.class)).collect(Collectors.toList());
+
+      res.setMembers(findMemberList);
       res.setCode(ResultCode.Success);
       return res;
     }
